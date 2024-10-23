3d4bdcdd61ab290ef56ee2cfdc680dadcf4b5b43
BrayanRuiz77 <andresbrayanpe@gmail.com>
2024-10-23 18:48:15 -0500

    ad

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Es mejor usar static const Color en lugar de solo static const
-  static const Color primary = Color(0xFFFF6B6B);
-  static const Color secondary = Color(0xFFFF8484);
+  static const Color primary = Color.fromARGB(255, 248, 102, 102);
+  static const Color secondary = Color.fromARGB(255, 248, 123, 123);
   static const Color background = Colors.white;
-  static const Color text = Color(0xFF2D3436);
-  static const Color grey = Color(0xFFF1F2F6);
-  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color text = Color.fromARGB(255, 45, 52, 54);
+  static const Color grey = Color.fromARGB(255, 241, 242, 246);
+  static const Color lightGrey = Color.fromARGB(255, 249, 249, 249);
 }

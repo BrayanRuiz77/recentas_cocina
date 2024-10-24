59b90e8959bff546148fc49b6581eae0384e4f7e
BrayanRuiz77 <andresbrayanpe@gmail.com>
2024-10-23 20:33:30 -0500

    aws

--- lib/widgets/custom_tab_bar.dart
+++ lib/widgets/custom_tab_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../theme/app_colors.dart';
 
 class CustomTabBar extends StatelessWidget implements PreferredSizeWidget {
   final TabController controller;
